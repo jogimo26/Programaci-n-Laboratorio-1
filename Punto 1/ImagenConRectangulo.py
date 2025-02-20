a135fc005905e9c89eeb1b7166c119e224043f5f
jogimo26 <jogimo26@gmail.com>
2025-02-20 17:46:25 -0500

    Add files via upload

---
+++ Punto 1/ImagenConRectangulo.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy
+
+# Resolución de la imagen (En y,x)
+dimy = 680
+dimx = 480
+
+# Puntos (en x,y) para el cuadrado
+iniciocuad = (240,140)
+fincuad = (440,340)
+
+# Se hace la imagen por medio de una matriz llena de ceros, usando la función numpy.zeros(). Se usa el tipo de datos uint8 porque va desde 0 a 255 para representar de negro a blanco
+img = numpy.zeros((dimx,dimy), numpy.uint8)
+
+# Se hace el rectángulo
+cv2.rectangle(img,iniciocuad,fincuad,255,-1)
+
+# Mostrar la imagen
+cv2.imshow("imagen",img)
+
+# Final, espera a que se presione cualquier tecla y se cierran todas las ventanas
+cv2.waitKey()
+cv2.destroyAllWindows()
